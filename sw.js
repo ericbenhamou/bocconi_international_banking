a1f6ac9982a8859643c77d0d44ca09e72bfe07b1
ericbenhamou <eric.famillebenhamou@gmail.com>
2026-07-14 15:41:21 +0200

    Refresh Pages cache for subject update

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
+const CACHE = "bocconi-study-desk-v7";
+const ASSETS = ["./", "./index.html", "./styles.css?v=7", "./questions.js?v=7", "./strategy_questions.js?v=7", "./app.js?v=7", "./recovery.js?v=7", "./manifest.webmanifest"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match("./index.html"))));
+});
 const CACHE = "banking-desk-v6";
 const ASSETS = ["./", "./index.html", "./styles.css?v=6", "./questions.js?v=6", "./app.js?v=6", "./recovery.js?v=6", "./manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
